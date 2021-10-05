af51a28c4da4a4368c1a465aa51da9e81b98e1d8
Jesus Miguel Quinto <jesuskinto@gmail.com>
2021-10-04 20:10:59 -0400

    [add] improvement on checkbox_demo

--- test_1_simple_form_demo.py
+++ test_1_simple_form_demo.py
@@ -6,16 +6,28 @@ from BaseClass import BaseClass
 class TestSimpleFormDemo(BaseClass):
     
     def test_single_input_field(self):
+
+        # First Let us try be very simple with only one input field and a Button
+        # Enter your message
+        # Click on 'Show Message' button to display message entered in input field
+
         url = "https://www.seleniumeasy.com/test/basic-first-form-demo.html"
         self.driver.get(url)
         message = "hellow world"
         self.driver.find_elements_by_xpath("//input[@id='user-message']")[0].send_keys(message)
         self.driver.find_elements_by_xpath("//button[contains(text(),'Show Message')]")[0].click()
         user_message = self.driver.find_element_by_css_selector("#user-message #display").text
+        
         assert user_message == message
 
 
     def test_two_input_fields(self):
+
+        # First Let us try with Two input fields and a Button
+        # Enter Value for a
+        # Enter Value for b
+        # Click on 'Get Total' button to display the sum of two numbers 'a and b'
+
         url = "https://www.seleniumeasy.com/test/basic-first-form-demo.html"
         self.driver.get(url)
         num1 = 2
@@ -24,4 +36,5 @@ class TestSimpleFormDemo(BaseClass):
         self.driver.find_element_by_id("sum2").send_keys(num2)
         self.driver.find_elements_by_xpath("//button[contains(text(),'Get Total')]")[0].click()
         total = int(self.driver.find_element_by_id("displayvalue").text)
+        
         assert total == (num1 + num2)
--- test_2_check_box_demo.py
+++ test_2_check_box_demo.py
@@ -1,3 +1,9 @@
+# This would be a basic example to start with checkboxes using selenium.
+
+# The HTML input "checkbox" is an input element to enter an array of different
+# values. Eash input type checkbox has value attribute which is used to define
+# the value submitted by the checkbox.
+
 from selenium import webdriver
 import pytest
 from BaseClass import BaseClass
@@ -7,6 +13,9 @@ class TestCheckBoxDemo(BaseClass):
     
 
     def test_single_checkbox_demo(self):
+        
+        # Clicking on the checkbox will display a success message. Keep an eye on it
+
         url = 'https://www.seleniumeasy.com/test/basic-checkbox-demo.html'
         self.driver.get(url)
         checkbox = self.driver.find_elements_by_id("isAgeSelected")[0]
@@ -15,14 +24,26 @@ class TestCheckBoxDemo(BaseClass):
 
 
     def test_two_input_fields(self):
+
+        # Check the below points before automating
+
         url = 'https://www.seleniumeasy.com/test/basic-checkbox-demo.html'
         self.driver.get(url)
         checkboxs = self.driver.find_elements_by_css_selector(".cb1-element")
-        
-        count = 0
-        for checkbox in checkboxs:
-            checkbox.click()
-            if checkbox.is_selected():
-                count +=1 
 
-        assert len(checkboxs) == count
+        # Click on 'Check All' to check all checkboxes at once.
+        self.driver.find_element_by_id("check1").click()
+        for checkbox in checkboxs:
+            if not checkbox.is_selected():
+                assert False
+
+        # When you check all the checkboxes, button will change to 'Uncheck All'
+        if (self.driver.find_element_by_id("check1").get_attribute('value') != 'Uncheck All'):
+            assert False
+
+        # When you uncheck at least one checkbox, button will change to 'Check All'
+        checkboxs[0].click()
+        if (self.driver.find_element_by_id("check1").get_attribute('value') != 'Check All'):
+            assert False
+
+        assert True
